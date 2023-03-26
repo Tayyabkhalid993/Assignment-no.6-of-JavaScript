7473e7113ca33f878e739b5f7b6692b6fec8feb3
Muhammad Tayyab Khalid <85389439+Tkhalid993@users.noreply.github.com>
2023-03-26 23:29:16 +0500

    Update Question no_2.js

--- Question no_2.js
+++ Question no_2.js
@@ -1,3 +1,12 @@
+/*
+*Assignment For Session 6:*
+
+
+2-Write a function that takes a number as an argument and returns true if the number is even, and false if it's odd
+
+
+*/
+
 function num(a) {
    b = a%2;
   if(b==0){
